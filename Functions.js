aade588498c52b75675bbc62cb5f459c98958257
SlinkyMation <jmmorris2@mail.bradley.edu>
2016-08-13 13:20:04 -0500

    More Good Stuff

--- Functions.js
+++ Functions.js
@@ -1,11 +1,13 @@
-// $(function() {
-//   var h1Height = $('h1').height(); // get height of h1 tag
-//   $('li a').click(function (e) {
-//       e.preventDefault();
-//       var target = $(this.hash);
-//       $('html, body').animate({
-//           scrollTop: target.offset().top - h1Height // scroll to h3 minus height of h1
-//       }, 1000);
-//       return false;
-//   });
-// });
+$(function() {
+  var h1Height = $('h1').height(); // get height of h1 tag
+  var toptabsHeight = $('ul.toptabs').height(); // get height of h1 tag
+  var total = h1Height + toptabsHeight;
+  $('.sidetabs li a').click(function (e) {
+      e.preventDefault();
+      var target = $(this.hash);
+      $('html, body').animate({
+          scrollTop: target.offset().top - total // scroll to h3 minus height of h1
+      }, 1000);
+      return false;
+  });
+});
